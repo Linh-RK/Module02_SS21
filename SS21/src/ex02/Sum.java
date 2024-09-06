0825deebb23f52382fccf1156b0c8341df20fff0
Linh Linh <pttlinh101193@gmail.com>
2024-09-06 08:41:52 +0700

    Add files via upload

--- SS21/src/ex02/Sum.java
+++ SS21/src/ex02/Sum.java
@@ -1,4 +1,25 @@
 package ex02;
 
 public class Sum {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(){
+            public void run(){
+                int sum = 0;
+                for(int i=1;i<=1000;i++){
+//                    System.out.println(i);
+                    sum+=i;
+                    if(i==10){
+                        System.out.println("End");
+                    }
+                try{
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                }
+                System.out.println(sum);
+            }
+        };
+        t1.start();
+    }
 }
